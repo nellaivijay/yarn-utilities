44e50526a19c6e8a70976dcb929c9c2eaa5fe8f1
ramdov <nellaivijay@gmail.com>
2016-01-16 16:07:16 -0500

    First Commit

--- long_runningJobs.py
+++ long_runningJobs.py
@@ -8,14 +8,14 @@ apps = []
 out = ""
 err = ""
 proc = ""
- 
+
 
 def running_apps(appid):
     command = "yarn application -status " + appid
     proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out, err = proc.communicate()
     lines = out.split("\n")
-    app_time = datetime.datetime.fromtimestamp(lines[4] / 1e3)
+    app_time = datetime.datetime.fromtimestamp(int(lines[4]) / 1e3)
     print  lines[1].split(":")[1], lines[2].split(":")[1], lines[4].split(":")[1], lines[6].split(":")[1], \
         lines[8].split(":")[1], lines[9].split(":")[1], app_time, (current_time - app_time)
 
