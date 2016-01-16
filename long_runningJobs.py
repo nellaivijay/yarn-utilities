48861b064764c8ac1a0970c1280603538a2c78d7
ramdov <nellaivijay@gmail.com>
2016-01-16 15:19:35 -0500

    First Commit

--- long_runningJobs.py
+++ long_runningJobs.py
@@ -13,8 +13,8 @@ def running_apps(appid):
     proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out, err = proc.communicate()
     lines = out.split("\n")
-    print lines[1], lines[1].split(":")[1], , lines[2].split(":")[1], , lines[4].split(":")[1], lines[5].split(":")[1], \
-    lines[7].split(":")[1], lines[9].split(":")[1]
+    print lines[1], lines[1].split(":")[1], lines[2].split(":")[1], lines[4].split(":")[1], lines[5].split(":")[1], \
+        lines[7].split(":")[1], lines[9].split(":")[1]
 
 
 def accepted_apps(appid):
